d89fe8edda309e3efbdeb9307d8862cbbc1ab9ca
otherworldindhu <80580975+otherworldindhu@users.noreply.github.com>
2021-09-13 16:38:01 +0530

    Create area of rectangle.cpp

---
+++ EXPT1/area of rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+    float area,l,b;
+    
+
+    std::cout << "enter l and b";
+    std::cin>>l>>b;
+
+    area=l*b;
+    std::cout<<"Area:"<<area;
+
+    return 0;
+}
